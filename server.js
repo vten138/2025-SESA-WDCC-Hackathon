2c536afd223a2c53b6d4e26f25625f3b4484c609
Victoria Tenitskaya <vten138@aucklanduni.ac.nz>
2025-08-02 12:41:46 +1200

    Added server file

---
+++ server.js
@@ -0,0 +1,13 @@
+// server.js
+const express = require('express');
+const path = require('path');
+
+const app = express();
+const PORT = 3000;
+
+// Serve static files (HTML, CSS, JS)
+app.use(express.static(path.join(__dirname)));
+
+app.listen(PORT, () => {
+  console.log(`Server running at http://localhost:${PORT}`);
+});
