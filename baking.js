d0600cc54001d8b0b4a56994366bcb77b9eed676
kwal133 <kwal133@aucklanduni.ac.nz>
2025-08-02 19:26:27 +1200

    updated opening oven code

--- baking.js
+++ baking.js
@@ -1,14 +1,17 @@
-// Get elements
 const oven = document.getElementById("oven-closed");
 
-// Oven click handler
 oven.addEventListener("click", () => {
+  // Prevent multiple clicks
+  if (oven.dataset.clicked === "true") return;
+  oven.dataset.clicked = "true";
+
+  // Show opening animation
   oven.src = "images/Cake-images-png/oven-opening.png";
 
   setTimeout(() => {
     oven.src = "images/Cake-images-png/oven-open.png";
     oven.classList.add("dropZone");
-    oven.id = "oven-open";
+    oven.id = "oven-open"; // Update ID
     console.log("Oven is now open");
   }, 2000);
 
